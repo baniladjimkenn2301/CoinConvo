c3217278be4b915a9716e00aaa2c697d974f433f
baniladjimkenn2301 <137855877+baniladjimkenn2301@users.noreply.github.com>
2023-11-30 06:55:48 +0800

    deleted details

--- script.js
+++ script.js
@@ -4,37 +4,23 @@ function updateNumberLabel() {
     document.getElementById('numberLabel').innerText = currentNumber;
 }
 
-function simulateAsyncOperation(callback) {
-    // Simulate an asynchronous operation with a delay
-    setTimeout(callback, 1000); // 1000 milliseconds (1 second) for simulation
-}
 
 function increment() {
-    simulateAsyncOperation(function() {
-        currentNumber++;
-        updateNumberLabel();
-        // You can add code here to send the updated value to your embedded system or Firebase.
-    });
+    currentNumber++;
+    updateNumberLabel();
+    // You can add code here to send the updated value to your embedded system or Firebase.
 }
 
 function decrement() {
-    simulateAsyncOperation(function() {
-        currentNumber--;
-        updateNumberLabel();
-        // You can add code here to send the updated value to your embedded system or Firebase.
-    });
+    currentNumber--;
+    updateNumberLabel();
+    // You can add code here to send the updated value to your embedded system or Firebase.
 }
 
 function moveLeft() {
-    simulateAsyncOperation(function() {
-        // Add logic for left arrow action if needed.
-    });
+    // Add logic for left arrow action if needed.
 }
 
 function moveRight() {
-    simulateAsyncOperation(function() {
-        // Add logic for right arrow action if needed.
-    });
+    // Add logic for right arrow action if needed.
 }
-
-// Continue with your existing JavaScript code...
