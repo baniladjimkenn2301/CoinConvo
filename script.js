8e1ce6a086bcbf2fa25e025913752cb089a32292
baniladjimkenn2301 <137855877+baniladjimkenn2301@users.noreply.github.com>
2023-12-05 12:36:47 +0800

    Bill accepted

--- script.js
+++ script.js
@@ -1,29 +1,38 @@
-let currentNumber = 0;
+var firebaseConfig = {
+    apiKey: "AIzaSyA42CXT-fDFcC5HDPPh46shNHhMx8c2e0s",
+    authDomain: "coinconvo-5cf2c.firebaseapp.com",
+    databaseURL: "https://coinconvo-5cf2c-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "coinconvo-5cf2c",
+    storageBucket: "coinconvo-5cf2c.appspot.com",
+    messagingSenderId: "507424099482",
+    appId: "1:507424099482:web:6f69f7c9c26a7c56f2e323",
+    measurementId: "G-2T07ZJNHMN"
+  };
+  //Initialize Database
+  firebase.initializeApp(firebaseConfig);
 
-function updateNumberLabel() {
-    document.getElementById('numberLabel').innerText = currentNumber;
-}
-
-
-function increment() {
-    currentNumber++;
-    updateNumberLabel();
-    // You can add code here to send the updated value to your embedded system or Firebase.
-}
-
-function decrement() {
-    currentNumber--;
-    updateNumberLabel();
-    // You can add code here to send the updated value to your embedded system or Firebase.
-}
-
-function moveLeft() {
-    // Add logic for left arrow action if needed.
-}
-
-function moveRight() {
-    // Add logic for right arrow action if needed.
-}
+  $(document).ready(function () {
+    var database = firebase.database();
+    var totalAmountRef = database.ref('totalAmount');
+  
+    // Check if the 'totalAmount' node exists, if not, initialize it with a default value
+    totalAmountRef.once('value', function (snapshot) {
+      if (!snapshot.exists()) {
+        // 'totalAmount' doesn't exist, initialize it with a default value (e.g., 0)
+        totalAmountRef.set(0);
+      }
+    });
+  
+    totalAmountRef.on('value', function (snapshot) {
+      var totalAmount = snapshot.val();
+      updateTotalAmountInHTML(totalAmount);
+    });
+  
+    function updateTotalAmountInHTML(amount) {
+      // Update the HTML with the new total amount
+      $('.total-amount').text(amount + " PHP");
+    }
+  });
 
 let popup= document.getElementById("popup");
 function openPopup(){
@@ -31,4 +40,13 @@ function openPopup(){
 }
 function closePopup(){
     popup.classList.remove("open-popup")
+}
+
+// Add the following code to update Firebase when a new bill is detected
+var database = firebase.database();
+var totalAmountRef = database.ref('totalAmount');
+
+// Assuming you have a function to update the total amount on Firebase
+function updateTotalAmountOnFirebase(newTotalAmount) {
+  totalAmountRef.set(newTotalAmount);
 }
